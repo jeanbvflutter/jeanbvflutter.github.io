2a509d1753bf0b6f24a8d2ab426ad04c5d9d7094
jeanbaptiste <jeanbaptiste.verroken@gmail.com>
2021-04-07 18:27:35 +0100

    basic ui

--- lib/views/mainpage.dart
+++ lib/views/mainpage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
+import 'package:flutter/services.dart';
+
 // Statefulwidget is mutable. It can be drawn multiple times within its lifetime.
 // They are widget that can change their state multiple times and can be redrawn on the screen any number of times while to app is in action.
 // "State" means data. The date can be changed during the lifetime of the widget without rebuilding it.
@@ -12,11 +14,74 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
+Text textWidget(String someText, textColor) {
+  return Text(
+    someText,
+    style: TextStyle(color: textColor),
+    textAlign: TextAlign.center,
+  );
+}
+
+Container headerWidget(String someText) {
+  return Container(
+    height: 50,
+    margin: EdgeInsets.only(bottom: 15),
+    child: Center(child: textWidget(someText, Colors.white)),
+    decoration: BoxDecoration(color: Colors.blueGrey),
+  );
+}
+
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: Text("Eleena Meter Activation")),
+      body: Container(
+        margin: EdgeInsets.all(5),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Center(child: headerWidget("Register meters")),
+                    Center(
+                        child: TextField(
+                      // inputFormatters: [
+                      //   FilteringTextInputFormatter.deny(RegExp(r'[0-9]')),
+                      // ],
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Enter meter serial number'),
+                    )),
+                    Container(
+                      margin: EdgeInsets.only(top: 5),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: ButtonTheme(
+                          minWidth: 150.0,
+                          height: 50.0,
+                          child: RaisedButton(
+                            child: Text('Investigate meter',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 15,
+                                    color: Colors.white)),
+                            onPressed: () {
+                              print("Press");
+                            },
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
